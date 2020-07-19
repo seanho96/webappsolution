000a0bd637762937613dc3272172bf53dec77e9d
Sean <seanho96@yahoo.com>
2020-07-19 20:42:11 +0800

    Updates

---
+++ precache-manifest.1f70ed3463bfdcb4cfe0664786b33bab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987564abcc57a9da3e6e1ae9a65114dd",
+    "url": "/webappsolution/index.html"
+  },
+  {
+    "revision": "eac080d0b0a863f5132c",
+    "url": "/webappsolution/static/css/main.54a59f75.chunk.css"
+  },
+  {
+    "revision": "d306361e9ee0bb8a13f5",
+    "url": "/webappsolution/static/js/2.39e9ec04.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/webappsolution/static/js/2.39e9ec04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eac080d0b0a863f5132c",
+    "url": "/webappsolution/static/js/main.e1331c96.chunk.js"
+  },
+  {
+    "revision": "6c9f1f2203059d71581f",
+    "url": "/webappsolution/static/js/runtime-main.0221198f.js"
+  },
+  {
+    "revision": "1b483f5f3e73468f0b48a40dcf400b13",
+    "url": "/webappsolution/static/media/Group 3.1b483f5f.svg"
+  },
+  {
+    "revision": "0e48089ff38fb96681fd068935617a77",
+    "url": "/webappsolution/static/media/hero.0e48089f.svg"
+  }
+]);
