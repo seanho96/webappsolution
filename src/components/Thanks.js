16189230356d998ac8de3e90c874c507e25e9271
Sean <seanho96@yahoo.com>
2020-08-02 21:02:50 +0800

    Modified package.json host settings

--- src/components/Thanks.js
+++ src/components/Thanks.js
@@ -5,11 +5,15 @@ const Thanks = () => {
 
   const history = useHistory();
   const { name } = useParams();
+  const goBack = () => {
+    history.goBack();
+  }
 
   console.log(history);
 
   return (
     <React.Fragment>
+      <button className="back" onClick={goBack}> &larr; Go Back</button>
       <div className="thanks">
         <p>Thanks <strong>{name} !!</strong></p>
         <p>You joined <strong>{history.location.state}</strong></p>
